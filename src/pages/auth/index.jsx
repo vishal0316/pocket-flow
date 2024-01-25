784b066b933f5055cd890e0708c61b5ae404f929
vishal0316 <vr987195@gmail.com>
2024-01-26 00:03:47 +0530

    expence tracker

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,9 +1,13 @@
 import { auth, provider } from "../../config/firebase-config";
 import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import pocketFlowSvg from "../../asset/money.svg";
 
 import "./style.css";
 
 export const Auth = () => {
+  let navigate = useNavigate();
+
   const SignInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider);
     const authInfo = {
@@ -13,11 +17,15 @@ export const Auth = () => {
       isAuth: true,
     };
     localStorage.setItem("auth", JSON.stringify(authInfo));
+    navigate("/expence-tracker");
   };
 
   return (
     <div className="login-page">
+      <div className="logo"> Pocket Flow</div>
+      <img src={pocketFlowSvg} alt="Pocket Flow Logo" className="logo-svg" />
       <p>Sign In With Google to Continue...</p>
+
       <button className="login-with-google-btn" onClick={SignInWithGoogle}>
         {" "}
         Sign In With Google{" "}
--- src/pages/expence-tracker/index.jsx
+++ src/pages/expence-tracker/index.jsx
@@ -1,9 +1,36 @@
+import "./style.css";
 export const ExpenceTracker = () => {
   return (
     <>
-      <div className="login-page">
-        <p>Sign In With Google to Continue</p>
-        <button className="login-with-google-btn"> Sign In With Google </button>
+      <div className="expense-tracker">
+        <div className="container">
+          <h1>Expense Tracker</h1>
+          <div className="balance card">
+            <h3>Your Balance</h3>
+            <h2>$1.00</h2>
+          </div>
+
+          <div className="summary">
+            <div className="income card">
+              <h4>Income</h4>
+              <p>$0.00</p>
+            </div>
+            <div className="expenses card">
+              <h4>Expenses</h4>
+              <p>$0.00</p>
+            </div>
+          </div>
+          <form className="add-transaction">
+            <input type="text" placeholder="Description" required />
+            <input type="number" placeholder="Amount" required />
+            <input type="radio" id="expense" value="expense" />
+            <label htmlFor="expense"> Expense</label>
+            <input type="radio" id="income" value="income" />
+            <label htmlFor="income"> Income</label>
+
+            <button type="submit"> Add Transaction</button>
+          </form>
+        </div>
       </div>
     </>
   );
