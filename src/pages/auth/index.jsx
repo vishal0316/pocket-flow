b8068db23aad46ae16e2e177e239d91e4cf20e1e
vishal0316 <vr987195@gmail.com>
2024-01-25 20:06:50 +0530

    auth component

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,3 +1,24 @@
+import { auth, provider } from "../../config/firebase-config";
+import { signInWithPopup } from "firebase/auth";
 export const Auth = () => {
-  return <div>hello</div>;
+  const SignInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    const authInfo = {
+      userID: result.user.uid,
+      name: result.user.displayName,
+      profilePhoto: result.user.photoURL,
+      isAuth: true,
+    };
+    localStorage.setItem("auth", JSON.stringify(authInfo));
+  };
+
+  return (
+    <div className="login-page">
+      <p>Sign In With Google to Continue</p>
+      <button className="login-with-google-btn" onClick={SignInWithGoogle}>
+        {" "}
+        Sign In With Google{" "}
+      </button>
+    </div>
+  );
 };
--- src/pages/expence-tracker/index.jsx
+++ src/pages/expence-tracker/index.jsx
@@ -1,3 +1,10 @@
 export const ExpenceTracker = () => {
-  return <div>hello expence tracker</div>;
+  return (
+    <>
+      <div className="login-page">
+        <p>Sign In With Google to Continue</p>
+        <button className="login-with-google-btn"> Sign In With Google </button>
+      </div>
+    </>
+  );
 };
